072e5e83e2ba03f189b9a879dcd136dcbede0a7e
Jules Spender <pro.jspender@gmail.com>
2019-05-26 08:34:48 -0700

    Version 1.00 build

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  publicPath: ‘<PersonalWebsite>’
-  css: {
-    // Enable CSS source maps.
-    sourceMap: process.env.NODE_ENV !== 'production'
-  }
+    css: {
+        sourceMap: process.env.NODE_ENV !== 'production'
+    }
 };
